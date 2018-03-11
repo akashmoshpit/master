717c4ae380126061b087d6a8b4ac15d2785d574f
Akash Gangrade <gangradeakash89@gmail.com>
2018-03-11 10:42:17 +0530

    all files pushed

---
+++ api/controllers/ArticleController.js
@@ -0,0 +1,14 @@
+/**
+ * ArticleController
+ *
+ * @description :: Server-side logic for managing articles
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	adore: function (req, res) {
+    res.send("hi how are you ??!");
+  }
+	//connnection:"mongodb"
+};
+
